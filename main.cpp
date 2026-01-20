a20c18fc27b8db32eb4b98a4f0f5c4ba56e146e3
HAMIOUI Yassine <Yassine.Hamioui@bordeaux-inp.fr>
2026-01-20 14:47:39 +0100

    Button Polling

--- main.cpp
+++ main.cpp
@@ -9,11 +9,25 @@ namespace {
 }
 using namespace std::chrono;
 
+const std::chrono::microseconds periods[] = {1s, 750ms, 500ms, 250ms, 100ms};
+
 Timer t;
+Ticker blink;
 int c = 0; //flag
-//DigitalIn  button(BUTTON1);
-InterruptIn button(BUTTON1);
+int c2 = 0; //flag 2
+//DigitalIn  button(BUTTON1); //Pour le poling
+InterruptIn button(BUTTON1); // Pour l'IRQ
 DigitalOut myled(LED1);
+
+
+
+void flip(){
+    myled =!myled;
+}
+
+void press(){
+    c2 = 1;
+}
 
 void high(){  
     myled = 1;
@@ -30,25 +44,38 @@ void low(){
 
 int main()
 {   
-    button.mode(PullDown);
-    button.rise(&high);
-    button.fall(&low);
+    //button.mode(PullDown);
+    //button.rise(&high);
+    //button.rise(&press);
+    //int i = 0;
+    //blink.attach(&flip, periods[i]);
+
+    //button.fall(&low);
     
     if (myled.is_connected()){
         printf("led initialized and connected!\n\r");
     }
     
     while (true) {
-        //printf("Groupe 2 : salut! \n");
+
+        /*if (c2){
+            c2 = 0;
+            i = (i + 1) % 5;
+            blink.detach();
+            blink.attach(&flip, periods[i]);
+
+        }*/
+        
+        printf("Groupe 2 : salut! \n");
         //ThisThread::sleep_for(PERIOD_MS / 2);
-        //myled = !myled;     // chaque boucle      
-        //myled = button;  // poling
+        //myled = !myled;     // chaque boucle pour le clignotement      
+        myled = button;  // poling
         /*ThisThread::sleep_for(PERIOD_MS / 4);
         myled.write(0);*/     
-        if(c){
+        /*if(c){
             c=0;
             printf("The time taken was %llu microseconds\n", duration_cast<microseconds>(t.elapsed_time()).count());
-        }
+        }*/
         ThisThread::sleep_for(PERIOD_MS / 4);
     }
 }
