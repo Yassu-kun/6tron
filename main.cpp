b3d57c3c909548182080fffea4f74b67bc044d77
HAMIOUI Yassine <Yassine.Hamioui@bordeaux-inp.fr>
2026-01-20 11:44:20 +0100

    exercice polling_intrpt_timer

--- main.cpp
+++ main.cpp
@@ -5,13 +5,50 @@
 #include "mbed.h"
 
 namespace {
-#define PERIOD_MS 2000ms
+#define PERIOD_MS 200ms
+}
+using namespace std::chrono;
+
+Timer t;
+int c = 0; //flag
+//DigitalIn  button(BUTTON1);
+InterruptIn button(BUTTON1);
+DigitalOut myled(LED1);
+
+void high(){  
+    myled = 1;
+    t.reset();
+    t.start();
+}
+
+
+void low(){
+    myled = 0;
+    t.stop();
+    c = 1;
 }
 
 int main()
-{
+{   
+    button.mode(PullDown);
+    button.rise(&high);
+    button.fall(&low);
+    
+    if (myled.is_connected()){
+        printf("led initialized and connected!\n\r");
+    }
+    
     while (true) {
-        printf("Alive! \n");
-        ThisThread::sleep_for(PERIOD_MS / 2);
+        //printf("Groupe 2 : salut! \n");
+        //ThisThread::sleep_for(PERIOD_MS / 2);
+        //myled = !myled;     // chaque boucle      
+        //myled = button;  // poling
+        /*ThisThread::sleep_for(PERIOD_MS / 4);
+        myled.write(0);*/     
+        if(c){
+            c=0;
+            printf("The time taken was %llu microseconds\n", duration_cast<microseconds>(t.elapsed_time()).count());
+        }
+        ThisThread::sleep_for(PERIOD_MS / 4);
     }
 }
